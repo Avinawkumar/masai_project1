35acd4e2e9305854f8dd7c4d1eba265344ea7894
Avinaw Kumar <0811kumaravinaw@gmail.com>
2022-10-21 19:54:25 +0530

    login_functionlaty_added

--- file1.js
+++ file1.js
@@ -1,2 +1,11 @@
-let login_user= "abc@gmil.com";
-let login_pass= "123@akv"
+let reg_email= "abc@gmil.com";
+let reg_pass= "123@akv";
+
+let enter_email = "abc@gmil.com";
+let enter_pass= "123@akv";
+
+if(reg_email === enter_email && reg_pass === enter_pass){
+    console.log("Login successful");
+} else{
+    console.log("Login failed");
+}
